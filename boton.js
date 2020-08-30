49a45055ae4feba22981c8b15bbb37ab34303cf0
Antac <angello_04_09@hotmail.com>
2020-08-29 20:50:14 -0500

    diseño terminado

--- boton.js
+++ boton.js
@@ -1,25 +1,54 @@
 const btn = document.getElementById("btn-no");
+/* Message */
+const buttonOpen = document.getElementById('btn-yes');
+const buttonClose = document.getElementById('close');
+const modal = document.getElementById('modal');
+const toggleActiveModal = () => modal.classList.toggle('active');
+buttonClose.addEventListener('click', toggleActiveModal);
+buttonOpen.addEventListener('click', toggleActiveModal);
 
+btn.addEventListener("mouseover", cordenadas)
 
-btn.addEventListener("mouseover",cordenadas)
-
-function cordenadas(){
-    let cordenadasBtn={
-        x: aleatorio(0,7)*60,
-       y: aleatorio(0,10)*40
-       }
-       console.log(cordenadasBtn);
-       pTop = cordenadasBtn.y + "px";
-       pLeft = cordenadasBtn.x + "px";
-       btn.style.left=pLeft;
-       btn.style.top=pTop;
-    btn.style.backgroundColor="red";
+function cordenadas() {
+    let cordenadasBtn = {
+        x: aleatorio(0, 7) * 60,
+        y: aleatorio(0, 10) * 40
+    }
+    console.log(cordenadasBtn);
+    /* if (cordenadasBtn.y == 0){
+        cordenadasBtn.y = cordenadasBtn.y + 16;
+     }else if (cordenadasBtn.x == 0){
+      cordenadasBtn.x = cordenadasBtn.x + 16;
+     } */
+    let pTop = cordenadasBtn.y + "px";
+    let pLeft = cordenadasBtn.x + "px";
+    btn.style.position = "absolute";
+    btn.style.left = pLeft;
+    btn.style.top = pTop;
 }
 
+btn.addEventListener("click", cordenadasCell)
 
-function aleatorio(min, maxi)
-{
-	var resultado;
-	resultado = Math.floor(Math.random()*(maxi - min + 1))+min;
-	return resultado;
+function cordenadasCell() {
+    let cordenadasBtn = {
+        x: aleatorio(0, 7) * 20,
+        y: aleatorio(0, 10) * 35
+    }
+    console.log(cordenadasBtn);
+    /* if (cordenadasBtn.y == 0){
+        cordenadasBtn.y = cordenadasBtn.y + 16;
+     }else if (cordenadasBtn.x == 0){
+      cordenadasBtn.x = cordenadasBtn.x + 16;
+     } */
+    let pTop = cordenadasBtn.y + "px";
+    let pLeft = cordenadasBtn.x + "px";
+    btn.style.position = "absolute";
+    btn.style.left = pLeft;
+    btn.style.top = pTop;
+}
+
+function aleatorio(min, maxi) {
+    var resultado;
+    resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
+    return resultado;
 }
